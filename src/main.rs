f7ab4f0feddc2d0b90476db510c5e28b3700b69e
kento tajiri <k.denjiry@gmail.com>
2020-04-09 01:11:47 +0900

    pass compile using fn pointer type instead of fn item type

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,43 @@ fn trans_true(_sv: SharedVars) -> bool {
 }
 
 fn main() {
-    let process = Process::new("label", "loc");
-    println!("{:?}", process);
+    let trans1 = Trans::new("read", "loc");
+    let process = vec![trans1];
+    let pp01: fn(SharedVars) -> SharedVars = p01;
+    // let pp01: fn(SharedVars) -> SharedVars = |sv| SharedVars {
+    //     x: sv.x,
+    //     t1: sv.x,
+    //     t2: sv.t2,
+    // };
+    let pp12: fn(SharedVars) -> SharedVars = p12;
+    let pp23: fn(SharedVars) -> SharedVars = p23;
+    let process_P = [
+        ("P0", ("read", "P1", trans_true, pp01)),
+        ("P1", ("inc", "P2", trans_true, pp12)),
+        ("P2", ("write", "P3", trans_true, pp23)),
+    ];
+}
+
+fn p01(sv: SharedVars) -> SharedVars {
+    SharedVars {
+        x: sv.x,
+        t1: sv.x,
+        t2: sv.t2,
+    }
+}
+
+fn p12(sv: SharedVars) -> SharedVars {
+    SharedVars {
+        x: sv.t1,
+        t1: sv.t1,
+        t2: sv.t2,
+    }
+}
+
+fn p23(sv: SharedVars) -> SharedVars {
+    SharedVars {
+        x: sv.x,
+        t1: sv.x,
+        t2: sv.t2,
+    }
 }
